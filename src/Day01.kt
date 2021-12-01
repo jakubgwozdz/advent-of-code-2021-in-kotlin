7ba366d1fc55d1f5273e9aeaf27b9606cac7e5ee
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2021-12-01 07:09:41 +0100

    day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,21 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }
+            .windowed(2)
+            .count { (a, b) -> b > a }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.asSequence().map { it.toInt() }
+            .windowed(3) { it.sum() }
+            .windowed(2)
+            .count { (a, b) -> b > a }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    expect(7) { part1(testInput) }
+    expect(5) { part2(testInput) }
 
     val input = readInput("Day01")
     println(part1(input))
--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,8 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+fun <T> expect(expected: T, block: ()->T) {
+    val actual = block();
+    check (expected == actual) { "expected $expected, but got $actual" }
+}
