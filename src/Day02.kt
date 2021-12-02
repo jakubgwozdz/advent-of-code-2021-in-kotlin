00e481e1a8a183290154da0a7f4eea25d3fcad9e
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2021-12-02 13:27:14 +0100

    day02

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,24 +1,24 @@
 fun main() {
-    data class Command(val dir: String, val value: Int)
+    data class Command(val word: String, val value: Int)
 
     val re = Regex("""^(\w+) (\d+)$""")
     val parse = { line: String ->
-        re.matchEntire(line)?.destructured?.let { (a, b) -> Command(a, b.toInt()) }
+        re.matchEntire(line)
+            ?.destructured
+            ?.let { (a, b) -> Command(a, b.toInt()) }
             ?: error("`$line` does not match `${re.pattern}`")
     }
 
     fun part1(input: List<String>): Int {
         data class Position(val x: Int, val y: Int)
-
-        val operation: (Position, Command) -> Position = { p, (dir, value) ->
-            when (dir) {
+        val operation: (Position, Command) -> Position = { p, (word, value) ->
+            when (word) {
                 "forward" -> p.copy(x = p.x + value)
                 "up" -> p.copy(y = p.y - value)
                 "down" -> p.copy(y = p.y + value)
-                else -> error("invalid command `$dir`")
+                else -> error("invalid command `$word`")
             }
         }
-
         return input.asSequence()
             .map(parse)
             .fold(Position(0, 0), operation)
@@ -28,12 +28,12 @@ fun main() {
     fun part2(input: List<String>): Int {
         data class Position(val x: Int, val y: Int, val aim: Int)
 
-        val operation: (Position, Command) -> Position = { p, (dir, value) ->
-            when (dir) {
+        val operation: (Position, Command) -> Position = { p, (word, value) ->
+            when (word) {
                 "forward" -> p.copy(x = p.x + value, y = p.y + p.aim * value)
                 "up" -> p.copy(aim = p.aim - value)
                 "down" -> p.copy(aim = p.aim + value)
-                else -> error("invalid command `$dir`")
+                else -> error("invalid command `$word`")
             }
         }
         return input.asSequence()
