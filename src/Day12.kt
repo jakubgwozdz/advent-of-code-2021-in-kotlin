521d6aa60d1709fb4642439e314678ba9c16d335
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2021-12-12 07:40:51 +0100

    day12 part 1+2 nice

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,77 +1,41 @@
 fun main() {
 
     val re = Regex("""^(\w+)-(\w+)$""")
-    val parse = { line: String ->
+    val parseLine = { line: String ->
         re.matchEntire(line)
             ?.destructured
             ?.let { (v1, v2) -> v1 to v2 }
             ?: error("`$line` does not match `${re.pattern}`")
     }
 
-
-    val paths = mutableSetOf<List<String>>()
-    var log = false
-
-    fun calculateAll(edges: Map<String, List<String>>, path: List<String>, maxVisits: Int) {
-//        println(path.joinToString("-") + " " + maxVisits)
-//        edges.forEach { println(it) }
-//        println()
-
-        val current = path.last()
-
-        var newMaxVisits = maxVisits
-        val newEdges = when {
-            current == "start" -> edges.mapValues { (_, l) -> l - current }
-            current.first()
-                .isLowerCase() && path.count { it == current } >= maxVisits -> edges.mapValues { (_, l) -> l - current }
-                .also { newMaxVisits = 1 }
-            else -> edges
-        }
-
-        if (current == "end") {
-            // workaround, todo: fix
-            val v = path.filter { it.first().isLowerCase() }.groupBy { it }
-                .mapValues { (_, v) -> v.size }
-                .count { (k, v) -> v == 2 }
-            if (v < 2) {
-                if (log) println(path.joinToString("-"))
-                paths += path
+    fun parse(input: List<String>) = buildMap<String, List<String>> {
+        input.map(parseLine)
+            .forEach {
+                compute(it.first) { _, l -> (l ?: emptyList()) + it.second }
+                compute(it.second) { _, l -> (l ?: emptyList()) + it.first }
             }
-        } else edges[current]!!.forEach { next ->
-            val newPath = path + next
-            calculateAll(newEdges, newPath, newMaxVisits)
+    }
+
+    fun calculateAll(
+        edges: Map<String, List<String>>,
+        current: String = "start",
+        visited: Collection<String> = hashSetOf(),
+        canVisitTwice: Boolean = false
+    ): Int {
+        val newVisited = visited + current
+        return if (current == "end") 1 else edges[current]!!.sumOf { next ->
+            when {
+                next.first().isUpperCase() -> calculateAll(edges, next, newVisited, canVisitTwice)
+                next !in visited -> calculateAll(edges, next, newVisited, canVisitTwice)
+                canVisitTwice && next != "start" -> calculateAll(edges, next, newVisited, false)
+                else -> 0
+            }
         }
     }
 
-    fun part1(input: List<String>): Int {
-        val edges = buildMap<String, List<String>> {
-            input.map(parse)
-                .forEach {
-                    compute(it.first) { _, l -> (l ?: emptyList()) + it.second }
-                    compute(it.second) { _, l -> (l ?: emptyList()) + it.first }
-                }
-        }
-        paths.clear()
+    fun part1(input: List<String>) = calculateAll(parse(input))
 
-        calculateAll(edges, listOf("start"), 1)
-
-        return paths.size
-    }
-
-    fun part2(input: List<String>): Int {
-        val edges = buildMap<String, List<String>> {
-            input.map(parse)
-                .forEach {
-                    compute(it.first) { _, l -> (l ?: emptyList()) + it.second }
-                    compute(it.second) { _, l -> (l ?: emptyList()) + it.first }
-                }
-                        }.mapValues { (k,v)->v.sorted() }
-        paths.clear()
-        log = true
-        calculateAll(edges, listOf("start"), 2)
-
-        return paths.size
-    }
+    fun part2(input: List<String>) = calculateAll(parse(input), canVisitTwice = true)
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day12_test")
