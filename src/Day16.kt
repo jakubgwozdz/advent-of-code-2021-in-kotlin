943385b90af8fb7118afaa9bb5d799df32a5b592
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2021-12-16 16:51:02 +0100

    day16 overengineering

--- src/Day16.kt
+++ src/Day16.kt
@@ -11,67 +11,59 @@ fun main() {
         else -> TODO()
     }
 
-    data class Parser(val src: String, var pos: Int = 0) {
+    data class Parser(private val src: String, private var pos: Int = 0) {
 
-        fun read(size: Int): String = src.substring(pos, pos + size).also { pos += size }
+        private fun read(size: Int): String = src.substring(pos, pos + size).also { pos += size }
+        private fun readInt(size: Int) = read(size).toInt(2)
+        private fun readHeader() = readInt(3) to readInt(3)
 
-        private fun parseLiteral(): Long {
-            val builder = StringBuilder()
-            do {
-                val next = read(5)
-                builder.append(next.drop(1))
-            } while (next[0] == '1')
+        private fun readLiteral() = sequence {
+            var next = true
+            while (next) {
+                next = read(1) == "1"
+                yield(readInt(4))
+            }
+        }.fold(0L) { acc, i -> acc * 65536 + i }
 
-            return builder.toString().toLong(2)
-        }
-
-        private fun <T> parseOperation(op: () -> T) = sequence {
+        private fun <T> readOperation(op: () -> T) = sequence {
             when (read(1)) {
-                "0" -> (readInt(15) + pos).let { while (pos < it) this.yield(op()) }
+                "0" -> (pos + readInt(15)).let { while (pos < it) this.yield(op()) }
                 "1" -> repeat(readInt(11)) { this.yield(op()) }
                 else -> error("wtf")
             }
         }
 
         fun decodeVersion(): Int {
-            val version = readInt(3)
-            return when (readInt(3)) {
-                4 -> version.also { parseLiteral() }
-                else -> version + parseOperation(this::decodeVersion).sum()
+            val (version, type) = readHeader()
+            return when (type) {
+                4 -> version.also { readLiteral() }
+                else -> version + readOperation(this::decodeVersion).sum()
             }
         }
 
         fun decodeValue(): Long {
-            readInt(3)
-            return when (val type = readInt(3)) {
-                4 -> parseLiteral()
-                else -> parseOperation(this::decodeValue).reduce(operation(type))
+            val (version, type) = readHeader()
+            return when (type) {
+                4 -> readLiteral()
+                else -> readOperation(this::decodeValue).reduce(operation(type))
             }
         }
 
-        private fun readInt(size: Int) = read(size).toInt(2)
-
     }
 
-    fun createProcess(hex: String): Parser {
-        val src = hex.flatMap { c ->
-            c.digitToInt(16).toString(2).padStart(4, '0').map { it }
-        }.joinToString("")
-        return Parser(src)
+    fun hex2bin(hex: String) = buildString {
+        hex.forEach { c -> append(c.digitToInt(16).toString(2).padStart(4, '0')) }
     }
 
-    fun part1(input: String) = createProcess(input).decodeVersion()
+    fun part1(input: String) = Parser(hex2bin(input)).decodeVersion()
 
-    fun part2(input: String) = createProcess(input).decodeValue()
+    fun part2(input: String) = Parser(hex2bin(input)).decodeValue()
 
     // test if implementation meets criteria from the description, like:
     val input = readInput("Day16").first()
-    expect(31)
-    { part1("A0016C880162017C3686B18A3D4780").also { logWithTime(it) } }
+    expect(31) { part1("A0016C880162017C3686B18A3D4780").also { logWithTime(it) } }
     logWithTime(part1(input))
-    expect(7)
-    { part2("880086C3E88112").also { logWithTime(it) } }
-    expect(1)
-    { part2("9C0141080250320F1802104A08").also { logWithTime(it) } }
+    expect(7) { part2("880086C3E88112").also { logWithTime(it) } }
+    expect(1) { part2("9C0141080250320F1802104A08").also { logWithTime(it) } }
     logWithTime(part2(input))
 }
