0349b0f39be1bbd3acb1e6f9e629c39d46c72887
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2021-12-20 07:40:15 +0100

    day20

--- src/Day20.kt
+++ src/Day20.kt
@@ -1,57 +1,56 @@
 fun main() {
 
-    data class Image(val input: List<String>, val outside: Char) {
-        operator fun get(row: Int, column: Int): String {
+    data class Img(val data: List<String>, val outside: Char) {
+        operator fun get(row: Int, column: Int): Int {
             return (-1..1).joinToString("") { dr ->
                 val r = dr + row
-                if (r !in input.indices) "$outside$outside$outside"
+                if (r !in data.indices) "$outside$outside$outside"
                 else {
-                    val x = "$outside$outside$outside$outside$outside${input[r]}$outside$outside$outside$outside$outside"
-                    x.substring(column-1+5, column+2+5)
+                    val x =
+                        "$outside$outside$outside$outside$outside${data[r]}$outside$outside$outside$outside$outside"
+                    x.substring(column - 1 + 5, column + 2 + 5)
+                }
+            }.replace('.', '0').replace('#', '1')
+                .toInt(2)
+        }
+
+        fun enhanced(alg: String): Img {
+            val i2 = (-1..data.indices.last + 1).map { r ->
+                buildString {
+                    (-1..data[0].indices.last + 1).forEach { c ->
+                        append(alg[this@Img[r, c]])
+                    }
                 }
             }
+            return Img(i2, if (alg[0] == '.') '.' else if (outside == '.') '#' else '.')
         }
+
+        fun count() = data.sumOf { r -> r.count { it == '#' } }
     }
 
+    fun part1(input: List<String>): Int {
+        val alg = input.first()
+        val image = Img(input.drop(2), '.')
 
-fun String.enhance(image: Image): Image {
-    val i2 = (-1..image.input.indices.last+1).map { r ->
-        buildString {
-            (-1..image.input[0].indices.last+1).forEach { c->
-                val p = image[r,c]
-                val code = p.replace('.','0').replace('#','1')
-                    .toInt(2)
-                val v = this@enhance[code]
-                append(v)
-            }
-        }
+        val img1 = image.enhanced(alg)
+        val img2 = img1.enhanced(alg)
+
+        return img2.count()
     }
-    return Image(i2, if(this[0]=='.') '.' else if (image.outside == '.') '#' else '.')
-}
 
-fun part1(input: List<String>): Int {
-    val alg = input.first()
-    val image = Image(input.drop(2), '.')
+    fun part2(input: List<String>): Int {
+        val alg = input.first()
+        var image = Img(input.drop(2), '.')
 
-    val img1 = alg.enhance(image)
-    val img2 = alg.enhance(img1)
-
-    return img2.input.sumOf { r -> r.count { it == '#' } }
-}
-
-fun part2(input: List<String>): Int {
-    val alg = input.first()
-    var image = Image(input.drop(2), '.')
-
-    repeat(50) { image = alg.enhance(image)}
-    return image.input.sumOf { r -> r.count { it == '#' } }
-}
+        repeat(50) { image = image.enhanced(alg) }
+        return image.count()
+    }
 
 // test if implementation meets criteria from the description, like:
-val testInput = readInput("Day20_test")
-val input = readInput("Day20")
-expect (35) { part1(testInput).also { logWithTime(it) } }
-logWithTime(part1(input))
-expect (3351) { part2(testInput).also { logWithTime(it) } }
-logWithTime(part2(input))
+    val testInput = readInput("Day20_test")
+    val input = readInput("Day20")
+    expect(35) { part1(testInput).also { logWithTime(it) } }
+    logWithTime(part1(input))
+    expect(3351) { part2(testInput).also { logWithTime(it) } }
+    logWithTime(part2(input))
 }
