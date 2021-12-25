0bae78854a2fd473e8fdb97e58af7d666a1f8a67
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2021-12-25 20:16:47 +0100

    day23

--- src/Day23.kt
+++ src/Day23.kt
@@ -1,15 +1,10 @@
-import AmphipodType.*
-import RoomType.*
 import kotlin.math.absoluteValue
 
-internal enum class RoomType { Hallway, HallwayAbove, RoomA, RoomB, RoomC, RoomD, Wall }
-internal enum class AmphipodType(val roomType: RoomType, val col: Int, val cost: Int) {
-    A(
-        RoomA,
-        3,
-        1
-    ),
-    B(RoomB, 5, 10), C(RoomC, 7, 100), D(RoomD, 9, 1000)
+internal enum class AmphipodType(val col: Int, val cost: Int) {
+    A(3, 1),
+    B(5, 10),
+    C(7, 100),
+    D(9, 1000)
 }
 
 fun main() {
@@ -18,48 +13,99 @@ fun main() {
         override fun toString() = "$row:$col"
     }
 
-    fun Pos.roomType() = when {
-        row == 1 && col in listOf(3, 5, 7, 9) -> HallwayAbove
-        row == 1 && col in 1..11 -> Hallway
-        row in 2..3 && col == 3 -> RoomA
-        row in 2..3 && col == 5 -> RoomB
-        row in 2..3 && col == 7 -> RoomC
-        row in 2..3 && col == 9 -> RoomD
-        else -> Wall
-    }
+    data class State(val positions: Map<Pos, AmphipodType>, val cost: Int, val roomSize: Int) {
 
-    data class State(val positions: Map<Pos, AmphipodType>, val cost: Int) {
-        fun print() {
-            println("cost $cost")
-            repeat(5) { r ->
-                repeat(13) { c ->
-                    val p = Pos(r, c)
-                    print(
-                        when {
-                            p in positions -> positions[p]
-                            p.roomType() == Wall -> "#"
-                            else -> "."
-                        }
-                    )
-                }
-                println()
-            }
-        }
-
-        fun canMove(from: Int, to: Int): Boolean {
+        fun canMoveHallWay(from: Int, to: Int): Boolean {
             val (b, e) = listOf(from, to).sorted()
             return (b..e)
                 .filter { it != from }
                 .none { Pos(1, it) in positions }
         }
 
-        fun move(from: Pos, to: Pos): State {
-            val c = (from.col - to.col).absoluteValue + (from.row - to.row).absoluteValue
-            val amph = positions[from]!!
-//            println("moving $amph from $from to $to")
-            return State(positions - from + (to to amph), cost + c * amph.cost)
+        fun canMoveInOwnRoom(amph: AmphipodType) = (2..roomSize + 1).all { row ->
+            positions[Pos(row, amph.col)].let { it == null || it == amph }
         }
 
+        fun roomPositions(col: Int) = (roomSize + 1 downTo 2).map { Pos(it, col) }
+
+        fun lowestEmpty(col: Int) = roomPositions(col).first { it !in positions }
+        fun highestUsed(col: Int) = roomPositions(col).last { it in positions }
+
+        fun move(from: Pos, to: Pos): State {
+            val amph = positions[from]!!
+            val dist = dist(from, to)
+            val moveCost = dist * amph.cost
+            return State(positions - from + (to to amph), cost + moveCost, roomSize)
+        }
+
+        private fun dist(from: Pos, to: Pos) =
+            (from.col - to.col).absoluteValue + (from.row - to.row).absoluteValue
+
+        fun isSolved() = positions.all { (pos, amph) -> amph.col == pos.col }
+        fun minCost() = cost + positions
+            .filter { (pos, amph) ->
+                pos.col != amph.col || shouldClean(pos.col)
+            }
+            .entries.sumOf { (pos, amph) ->
+                val top = pos.copy(row = 1)
+                amph.cost * (2.coerceAtLeast(dist(pos, top) + dist(top, Pos(2, amph.col))))
+            }
+
+        fun shouldClean(col: Int) =
+            roomPositions(col).any { positions[it].let { a -> a != null && a.col != col } }
+
+    }
+
+    val hallways = listOf(1, 2, 4, 6, 8, 10, 11)
+
+    fun possibleMoves(s: State): List<State> = buildList {
+        val goingDown = hallways.map { Pos(1, it) }
+            .map { it to s.positions[it] }
+            .mapNotNull { (pos, amph) -> amph?.let { pos to it } }
+            .firstOrNull { (pos, amph) ->
+                s.canMoveHallWay(pos.col, amph.col) && s.canMoveInOwnRoom(amph)
+            }
+        if (goingDown != null) {
+            val (pos, amph) = goingDown
+            add(s.move(pos, s.lowestEmpty(amph.col)))
+        }
+//                .forEach { }
+        else {
+            (3..9 step 2).filter { col ->
+                s.roomPositions(col)
+                    .any { s.positions[it].let { a -> a != null && a.col != col } }
+            }.map { col ->
+                s.highestUsed(col).let { it to s.positions[it]!! }
+            }.forEach { (pos, amph) ->
+                hallways
+                    .filter { col -> s.canMoveHallWay(pos.col, col) }
+                    .forEach { col -> add(s.move(pos, Pos(1, col))) }
+            }
+        }
+    }
+
+    fun solve(initial: State): Int {
+
+        var result = Int.MAX_VALUE
+
+        val todo = Stack<State>()
+        todo.offer(initial)
+
+        while (todo.isNotEmpty()) {
+            val state = todo.poll()
+            if (state.isSolved()) {
+                if (state.cost < result) {
+                    result = state.cost
+                        .also { logWithTime("found $it") }
+                }
+            } else {
+                possibleMoves(state)
+                    .filter { it.cost < result }
+                    .filter { it.minCost() < result }
+                    .forEach { todo.offer(it) }
+            }
+        }
+        return result
     }
 
     fun part1(input: List<String>): Int {
@@ -70,89 +116,20 @@ fun main() {
                 }
             }
         }
-
-        val edges = buildSet {
-            (1..10).forEach { c ->
-                add((1 to c) to (1 to c + 1))
-                add((1 to c + 1) to (1 to c))
-            }
-            (3..9 step 2).forEach { c ->
-                add((1 to c) to (2 to c))
-                add((2 to c) to (1 to c))
-                add((2 to c) to (3 to c))
-                add((3 to c) to (2 to c))
-            }
-        }
-
-        val hallways = listOf(1, 2, 4, 6, 8, 10, 11)
-
-        fun possibleMoves(s: State): List<State> = buildList {
-            s.positions.forEach { (pos, amph) ->
-                when {
-                    pos.row == 3 && amph.roomType == pos.roomType() -> {} // all good, stop
-                    pos.row == 2 && amph.roomType == pos.roomType() && s.positions[pos.copy(row = 3)] == amph -> {} // all good, stop
-                    pos.row == 2 -> hallways
-                        .filter { col -> s.canMove(pos.col, col) }
-                        .forEach { col -> add(s.move(pos, Pos(1, col))) }
-                    pos.row == 3 && Pos(2, pos.col) !in s.positions -> hallways
-                        .filter { col -> s.canMove(pos.col, col) }
-                        .forEach { col -> add(s.move(pos, Pos(1, col))) }
-                    pos.row == 1 && s.canMove(pos.col, amph.col) -> {
-                        val p2 = Pos(2, amph.col)
-                        val p3 = Pos(3, amph.col)
-                        if (p3 !in s.positions) {
-                            add(s.move(pos, p3))
-                        } else if (s.positions[p3] == amph && p2 !in s.positions) {
-                            add(s.move(pos, p2))
-                        }
-                    }
-
-                }
-            }
-        }
-
-
-        val end = buildMap {
-            this[Pos(2, 3)] = A
-            this[Pos(3, 3)] = A
-            this[Pos(2, 5)] = B
-            this[Pos(3, 5)] = B
-            this[Pos(2, 7)] = C
-            this[Pos(3, 7)] = C
-            this[Pos(2, 9)] = D
-            this[Pos(3, 9)] = D
-        }
-
-
-        var solution:State? = null
-//        fun calc
-
-
-        val queue = Queue<State>()
-        queue.offer(State(pos, 0))
-
-        while (queue.isNotEmpty()) {
-            val state = queue.poll()
-//            state.print()
-            if (state.positions == end) {
-                if (solution == null || solution.cost > state.cost)
-                    solution = state
-                println(state.cost)
-            }
-            if (state.cost < (solution?.cost ?: Int.MAX_VALUE))
-                possibleMoves(state)
-                    .sortedBy { s->s.cost }//- AmphipodType.values().filter { state.positions[Pos(3,)) } }
-//                .also { println("adding ${it.size} states") }
-                    .forEach { queue.offer(it) }
-
-        }
-
-
-        return solution!!.cost
+        return solve(State(pos, 0, 2))
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+        val pos: Map<Pos, AmphipodType> = buildMap {
+            (3..9 step 2).forEach { c ->
+                this[Pos(2, c)] = AmphipodType.valueOf(input[2][c].toString())
+                this[Pos(3, c)] = AmphipodType.valueOf("  #D#C#B#A#"[c].toString())
+                this[Pos(4, c)] = AmphipodType.valueOf("  #D#B#A#C#"[c].toString())
+                this[Pos(5, c)] = AmphipodType.valueOf(input[3][c].toString())
+            }
+        }
+
+        return solve(State(pos, 0, 4))
     }
 
 // test if implementation meets criteria from the description, like:
@@ -160,24 +137,6 @@ fun main() {
     val input = readInput("Day23")
     expect(12521) { part1(testInput).also { logWithTime(it) } }
     logWithTime(part1(input))
-    expect(0) { part2(testInput).also { logWithTime(it) } }
+    expect(44169) { part2(testInput).also { logWithTime(it) } }
     logWithTime(part2(input))
 }
-
-
-//        repeat(5) { r ->
-//            repeat(13) { c ->
-//                print(
-//                    when (type(r, c)) {
-//                        Hallway -> "."
-//                        HallwayAbove -> "_"
-//                        RoomA -> "A"
-//                        RoomB -> "B"
-//                        RoomC -> "C"
-//                        RoomD -> "D"
-//                        Wall -> "#"
-//                    }
-//                )
-//            }
-//            println()
-//        }
